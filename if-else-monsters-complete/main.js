878b87e7e81b373404a9988e47489eb1a91b94d6
madison.mitschke <madison@mitschke.ca>
2017-05-17 21:05:38 +0200

    Madison's dance

--- if-else-monsters-complete/main.js
+++ if-else-monsters-complete/main.js
@@ -1,34 +1,87 @@
-function monsters() {
-	var choice1 = prompt("You are walking in the forest, when a monster jumps out. Do you FIGHT, PAY or RUN?","Type your answer here").toUpperCase();
-
-	if (choice1 === "FIGHT") {
-		var choice2 = prompt("Are you strong?","Yes or No").toUpperCase();
-		if (choice2 === "YES") {
-			alert("You beat the monster!");
+var fightChoice = function () {
+	var fightStrong = prompt("Are you strong?", "Yes or No").toUpperCase();
+	if (fightStrong === "YES") {
+		alert("You beat the monster!");
+	} else if (fightStrong === "NO") {
+		fightSmartFunc();
+	} else {
+		alert("Your input is invalid! :(");
+		fightChoice();
+	}
+};
+var fightSmartFunc = function () {
+	var fightSmart = prompt("Are you smart?", "Yes or No").toUpperCase();
+	if (fightSmart === "YES") {
+		alert("Smarty pants ;) You beat the monster!");
+	} else if (fightSmart === "NO") {
+		payChoice();
+	} else {
+		alert("Huh, I didn't get that...");
+		fightSmartFunc();
+	}
+};
+var payChoice = function () {
+	var payWallet = prompt("Do you have 100€ in your wallet?", "Yes or No").toUpperCase();
+	if (payWallet === "YES") {
+		alert("You beat the monster!");
+	} else if (payWallet === "NO") {
+		var payBank = prompt("Is there a bank close by?", "Yes or No").toUpperCase();
+		if (payBank === "YES") {
+			var payGetMoney = promt("Let's go get the money, honey!").toUpperCase();
+			if (payGetMoney === "YES") {
+				alert("You beat that greedy monster!")
+			} else if (payGetMoney === "NO") {
+				alert("Ahhh so you're a liar! No passing for you.")
+			} else {
+				alert("@#$%@% is that another language? Try again.")
+				prompt("Let's go get the money, honey!").toUpperCase();
+			}
+		} else if (payBank === "NO") {
+			alert("No money, not pass! Outta luck this time kiddo!")
 		} else {
-			var choice3 = prompt("Are you smart?","Yes or No").toUpperCase();
-			if(choice3 === "YES") {
-		      alert("I guess that'll do! You beat the monster - nice work!");
-		    } else {
-		      alert("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a monster. You lose!");
-		    }
+			alert("What was that!? I didn't understand.")
+			prompt("Is there a bank close by?", "Yes or No");
 		}
-
-	} else if (choice1 == "PAY") {
-		var choice2 = prompt("Are you rich?","Yes or No").toUpperCase();
-		if (choice2 === "YES") {
-			alert("Well I guess you can afford 100€!");
-		} else {
-			alert("Too bad! It's human for dinner!")
-		}
-	} else if (choice1 == "RUN") {
-		var choice2 = prompt("Are you fast?","Yes or No").toUpperCase();
-		if (choice2 === "YES") {
-			alert("Better get your running shoes on then!");
-		} else {
-			alert("This doesn't end well for you, tiny human!")
-		}
+	} else {
+		alert("Typo? Let's go back!")
+		payChoice();
 	}
 }
+var runChoice = function () {
+	var runFast = prompt("Are you fast?", "Yes or No").toUpperCase();
+	if (runFast === "YES") {
+		alert("You beat the monster!");
+	} else if (runFast === "NO") {
+		var runLong = prompt("Do you have endurance?", "Yes or No").toUpperCase();
+		if (runLong === "YES") {
+			alert("You beat the monster!");
+		} else if (runLong === "NO") {
+			alert("You shall not pass! :(")
+		} else {
+			alert("What was that? I didn't understand! :((");
+			prompt("Do you have endurance?", "Yes or No")
+		}
+	} else {
+		alert("I didn't get it!")
+		prompt("Are you fast?", "Yes or No")
+	}
 
+}
+function monsters() {
+	var playQuestions = prompt('You are walking in the forest, when a monster jumps out. "None shall pass!" he says, "Unless, of course, you pay me 100€" Do you FIGHT, PAY or RUN?', "Type your answer here").toUpperCase();
+
+	if (playQuestions === "FIGHT") {
+		fightChoice();
+
+	} else if (playQuestions == "PAY") {
+		payChoice();
+
+	} else if (playQuestions === "RUN") {
+		runChoice();
+
+	} else {
+		alert("Your input is invalid! :(");
+		monsters();
+	}
+}
 document.getElementById("monsters").onclick = monsters;
